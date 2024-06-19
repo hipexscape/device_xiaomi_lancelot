4c618471f8e879288a3bbac2472e7b7dae1b33ad
me-cafebabe <me_cafebabe@hotmail.com>
2024-06-19 10:01:54 +0530

    lancelot: Set ro.build.fingerprint
    * If we didn't set this, init's property_derive_build_fingerprint() function would generate a build fingerprint and store to the prop

--- lineage_lancelot.mk
+++ lineage_lancelot.mk
@@ -26,3 +26,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="lancelot-user 11 RP1A.200720.011 V12.5.6.0.RJCMIXM release-keys"
 
 BUILD_FINGERPRINT := Redmi/lancelot_global/lancelot:11/RP1A.200720.011/V12.5.6.0.RJCMIXM:user/release-keys
+PRODUCT_SYSTEM_PROPERTIES += ro.build.fingerprint=$(BUILD_FINGERPRINT)
