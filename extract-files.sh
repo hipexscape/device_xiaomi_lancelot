74a1e5ae68c567b8e26b05f4dcacec4fcfda2c81
Michael Bestas <mkbestas@lineageos.org>
2024-09-15 07:54:30 +0530

    lancelot: Sync extract scripts with templates
    Change-Id: Ie2a1ac2ec629caecfd9dbe4f23cc28c6171a6684

--- extract-files.sh
+++ extract-files.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
 function blob_fixup() {
     case "${1}" in
         vendor/bin/mnld)
+            [ "$2" = "" ] && return 0
             "$PATCHELF" --add-needed "libshim_sensors.so" "$2"
             ;;
         vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so)
+            [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             ;;
+        *)
+            return 1
+            ;;
     esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
 }
 
 # If we're being sourced by the common script that we called,
@@ -28,5 +38,6 @@ set -e
 export DEVICE=lancelot
 export DEVICE_COMMON=mt6768-common
 export VENDOR=xiaomi
+export VENDOR_COMMON=${VENDOR}
 
-"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
+"./../../${VENDOR_COMMON}/${DEVICE_COMMON}/extract-files.sh" "$@"
